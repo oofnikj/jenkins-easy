a0c6de4965ddca19473b31e81c2f0750e76a9baf
Jordan Sokolic <oofnik@gmail.com>
2020-12-04 11:52:08 +0200

    tf required_providers block

--- deploy/terraform/okta.tf
+++ deploy/terraform/okta.tf
@@ -1,4 +1,17 @@
-provider "okta" {}
+terraform {
+  required_providers {
+    okta = {
+      source  = "oktadeveloper/okta"
+      version = "~> 3.6"
+    }
+  }
+}
+
+provider "okta" {
+  org_name  = var.okta_org_name
+  base_url  = var.okta_base_url
+  api_token = var.okta_api_token
+}
 provider "local" {}
 
 variable "okta_org_name" {}
